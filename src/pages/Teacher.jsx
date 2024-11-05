88ac8e0ebace4891e84ca5b0dc567147771df281
akhilss235 <146330955+akhilss235@users.noreply.github.com>
2024-11-05 14:15:16 +0530

    table

--- src/pages/Teacher.jsx
+++ src/pages/Teacher.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Teacherstable from '../Table/Teacherstable'
 
 function Teacher() {
   return (
-    <div>Teacher</div>
+    <div>
+      <Teacherstable />
+    </div>
   )
 }
 
